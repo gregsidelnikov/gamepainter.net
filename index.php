270b1f715fcddb1899d3db346ad5d7d376eaf873
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-12-16 18:19:50 -0500

    Initial commi

---
+++ index.php
@@ -0,0 +1,100 @@
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<head>
+<title>Game Painter - Free Online Game Maker Software - No Download</title>
+<?php /* <link rel = "icon" href = "http://www.gamepainter.net/favicon.gif" /> */ ?>
+<META http-equiv="Content-Type" content="text/html;charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<META name = "description" content = "Welcome to Game Painter - The Official Game Maker software, there is no download because it's written in JavaScript language. Make games right in the browser."/>
+<META name = "keywords" content = "game,maker,software,tool,free,no,download,browser,js,javascript,tutorials"/>
+<meta name="language" content="">
+<meta http-equiv="content-language" content="en-US" />
+<meta name="verify-v1" content="---" /> <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
+<meta property="og:title" content="GamePainter.net - free online game maker software tool no download"/>
+<?php /* <meta property="og:type" content="article"/>
+<meta property="og:url" content="http://www.learnjquery.org/homepage/draft-dge1380.0162756006573.html"/>
+<meta property="og:image" content="http://www.learnjquery.org/Images/tutorial-article.png"/>
+<meta property="og:site_name" content=""/> */ ?>
+<meta property="fb:admins" content="gregsidelnikov"/>
+<script src = 'http://www.gamepainter.net/js/jquery.js' type = 'text/javascript'></script>
+<script src = 'http://www.gamepainter.net/js/ui.js' type = 'text/javascript'></script>
+<script src = 'http://www.gamepainter.net/script.js' type = 'text/javascript'></script>
+<link rel = 'stylesheet' type = 'text/css' href = 'http://www.gamepainter.net/css/style.css' />
+    <script type = "text/javascript">
+        /* Create global website's object */
+        window.w = 0; // responsive width
+        window.website = new Object();
+        website.url = 'http://www.gamepainter.net';
+        website.img_dir_name = 'Images';
+        website.bs_ip = '65.24.43.194';
+        website.this_ip = '2600:3c01::f03c:91ff:feae:69f9';
+        website.admin = 0;    </script>
+    <script type = "text/javascript">
+        /* Custom JavaScript */
+    </script>
+<style type="text/css">
+
+</style>
+<base href = "http://www.gamepainter.net" target="_blank">
+</head>
+    <script type = "text/javascript">
+		/* Template custom parameters */
+    </script>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    <script type = "text/javascript">
+    	function IsMobile() { if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) { window.mobile = true; $("body *").addClass("IncreaseFontSize"); } }
+    	function Resize() {
+//        	if (w < 900) { $("body").addClass("MiniSite"); } else { $("body").removeClass("MiniSite"); }
+    	}
+	    $(window).resize(function() { Resize(); });
+	    $(window).load(function() { Resize(); });
+	    $(document).ready(function() {
+	    	IsMobile();
+			Resize();
+	    });
+    </script>
+    <style type = "text/css">
+    body { font-family: Arial, sans-serif; font-size: 12px; margin: 0; padding: 0; background: #f1f1f1; } 
+    #Header { height: 48px; background: #fff; border-bottom: 1px solid #e8e8e8; }
+    .GameInfo { margin: 16px; margin-bottom: 8px; width: 200px; height: 190px; border: 0; float: left; }
+    #MenuOptions { width: 500px; margin: auto; }
+    .MenuOption { width: 75px; height: 28px; line-height: 24px; float: left; margin-left: 10px; margin-right: 10px; text-align: center; }
+    .MenuOption { border-bottom: 3px solid transparent; }
+    .MenuOption.Selected { border-bottom: 3px solid orange; }
+	.gamedesc { font-size: 11px; position: relative; font-family: Arial, sans-serif; }
+	.gameinfo { font-size: 11px;  }
+	.GameInfo a { color: blue; }
+	#Navigation { background: white; width: 100%; height: 31px; margin: 0 auto; position: relative; padding-top: 8px; }
+	.gamestat { margin-top: 3px; font-size: 11px; font-family: Arial; color: #777; }
+    #GameList { position: relative;  margin: 32px; height: auto; background: #fff; }
+    </style>
+</head>
+<body>
+	<div id = "Header">
+	    <img src = "http://www.gamepainter.net/grid.png" alt = "menu" />
+        <img src = "http://www.gamepainter.net/game-painter-logo.png" style = "margin-top: 5px;" alt = "Game Painter Logo"/>
+	    <img src = "http://www.gamepainter.net/userpic.png" alt = "user" style = "opacity: 0.5; position: absolute; top: 11px; right: 10px; border: 2px solid #333; border-radius: 777px;" />
+	</div>
+	<div id = "Navigation">
+		<div class = "MenuOption Selected">Home</div>
+		<?php /* <div class = "MenuOption">Popular</div> */ ?>
+	</div>
+	
+	<div id = "GameList">
+	
+		<div style = "padding: 16px;"><b>Game Painter</b> is free online 2D game maker software that you can use to <a href = "#">create your own game</a>, share it with your friends or browse and play games created by others.</div>
+	
+		<div class = "GameInfo">
+			<img src = "http://www.gamepainter.net/make-game.png" alt = "Add Game - Make Your Own Game"/>
+			<b><a href = "http://www.gamepainter.net/fFJKHD86vh2461">Make Your Own Game</a></b>
+			<div class = "gamedesc">Create your own 2D game, publish it for free on Game Painter and invite friends to play it.</div>
+			<div class = "gamestat">0 plays - now</div>
+		</div>
+
+		<?php include("gameblock.php"); ?>
+
+		<div style = "clear: both"></div>
+
+	</div>
+</body>
+</html>
