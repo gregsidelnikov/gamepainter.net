25d4fb039b24c4162acdf03e10e7b5be113cfffd
Greg Sidelnikov <greg.sidelnikov@gmail.com>
2016-12-16 19:51:48 -0500

    Sidebar adjustments

--- index.php
+++ index.php
@@ -43,12 +43,19 @@
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     <script type = "text/javascript">
 		function ToggleSidebar() {
-			$('body').removeClass("Sidebar");
-			if (window.sidebar == "true")  {
+
+			if (window.sidebar == "true") {
 				localStorage.setItem('sidebar', "false");
-			} else {
-				$('body').addClass("Sidebar");
+				window.sidebar = "false";
+				$('body').removeClass("Sidebar");
+				return;
+			}
+
+			if (window.sidebar == "false")  {
 				localStorage.setItem('sidebar', "true");
+				window.sidebar = "true";
+				$('body').addClass("Sidebar");
+				return;
 			}
 		}
 		function Sidebar() {
@@ -63,7 +70,12 @@
 		}
     	function IsMobile() { if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) { window.mobile = true; $("body *").addClass("IncreaseFontSize"); } }
     	function Resize() {
-//        	if (w < 900) { $("body").addClass("MiniSite"); } else { $("body").removeClass("MiniSite"); }
+			var w = $(window).width();
+        	if (w < 800) {
+				localStorage.setItem('sidebar', "false");
+				window.sidebar = "false";
+				$('body').removeClass("Sidebar");
+			}
     	}
 	    $(window).resize(function() { Resize(); });
 	    $(window).load(function() { Resize(); });
@@ -74,7 +86,7 @@
 	    });
     </script>
     <style type = "text/css">
-    body { font-family: Arial, sans-serif; font-size: 12px; margin: 0; padding: 0; background: #f1f1f1; } 
+    body { font-family: Arial, sans-serif; font-size: 12px; margin: 0; padding: 0; background: #f1f1f1; }
     #Header { height: 48px; background: #fff; border-bottom: 1px solid #e8e8e8; }
     .GameInfo { margin: 16px; margin-bottom: 8px; width: 200px; height: 190px; border: 0; float: left; }
     #MenuOptions { width: 500px; margin: auto; }
@@ -83,39 +95,46 @@
     .MenuOption.Selected { border-bottom: 3px solid orange; }
 	.gamedesc { font-size: 11px; position: relative; font-family: Arial, sans-serif; }
 	.gameinfo { font-size: 11px;  }
-	.GameInfo a { color: blue; }
+	.GameInfo a { color: #2388de; text-decoration: none; }
 	#Navigation { background: white; width: 100%; height: 31px; margin: 0 auto; position: relative; padding-top: 8px; }
 	.gamestat { margin-top: 3px; font-size: 11px; font-family: Arial; color: #777; }
     #GameList { position: relative;  margin: 32px; height: auto; background: #fff; }
 
 	#Sidebar { display: none; }
-	.sb-opt { padding: 5px; height: 30px; line-height: 32px; border-bottom: 1px solid #ddd; color: gray; }
+	.sb-opt { cursor: pointer; padding: 5px; height: 30px; line-height: 32px; border-bottom: 1px solid #ddd; color: gray; }
+	.sb-opt:hover { background: #555; color: white; }
 
-	body.Sidebar #GameList { margin-left: 280px; }
+	body.Sidebar #GameList { margin-left: 200px; }
 	body.Sidebar #Sidebar { display: block !important; }
+
+	#Footer a { color: gray; }
+
+	.icon { display: inline-block; width: 16px; height: 16px; border: 1px solid #ddd; vertical-align: middle; margin-top:-3px; margin-left: 6px; margin-right: 8px; }
+
+		a { color: #2388de;  }
 
     </style>
 </head>
 <body>
 	<div id = "Header">
-	    <img src = "http://www.gamepainter.net/grid.png" alt = "menu" onclick = "ToggleSidebar()" />
+	    <img src = "http://www.gamepainter.net/grid.png" alt = "menu" onclick = "ToggleSidebar()" style = "cursor: pointer;" />
         <img src = "http://www.gamepainter.net/game-painter-logo.png" style = "margin-top: 5px;" alt = "Game Painter Logo"/>
 	    <img src = "http://www.gamepainter.net/userpic.png" alt = "user" style = "opacity: 0.5; position: absolute; top: 11px; right: 10px; border: 2px solid #333; border-radius: 777px;" />
 	</div>
 
 	<div id = "Navigation">
 		<div style = "width: 300px; margin: 0 auto">
-			<div class = "MenuOption Selected">Home</div>
+			<div class = "MenuOption Selected">Browse</div>
+			<div class = "MenuOption" style = "min-width: 120px;">Create My Own Game</div>
 			<?php /* <div class = "MenuOption">Popular</div> */ ?>
 		</div>
 	</div>
 
-	<div id = "Sidebar" style = "position: absolute; top: 89px; left: 0; width: 250px; height: 100%; background: white;">
-		<div class = "sb-opt">Option 1</div>
-		<div class = "sb-opt">Option 2</div>
-		<div class = "sb-opt">Option 3</div>
-		<div class = "sb-opt">Option 4</div>
-		<div class = "sb-opt">Option 5</div>
+	<div id = "Sidebar" style = "position: absolute; top: 89px; left: 0; width: 180px; height: 100%; background: white;">
+		<div class = "sb-opt"><div class = "icon"></div> Home</div>
+		<div class = "sb-opt"><div class = "icon"></div> My Games</div>
+		<div class = "sb-opt"><div class = "icon"></div> Analytics</div>
+		<div class = "sb-opt"><div class = "icon"></div> Contact</div>
 	</div>
 	
 	<div id = "GameList">
@@ -123,7 +142,10 @@
 		<div style = "padding: 16px;"><b>Game Painter</b> is free online 2D game maker software that you can use to <a href = "#">create your own game</a>, share it with your friends or browse and play games created by others.</div>
 	
 		<div class = "GameInfo">
+
 			<img src = "http://www.gamepainter.net/make-game.png" alt = "Add Game - Make Your Own Game"/>
+			
+
 			<b><a href = "http://www.gamepainter.net/fFJKHD86vh2461">Make Your Own Game</a></b>
 			<div class = "gamedesc">Create your own 2D game, publish it for free on Game Painter and invite friends to play it.</div>
 			<div class = "gamestat">0 plays - now</div>
@@ -133,9 +155,14 @@
 		<?php include("gameblock.php"); ?>
 		<?php include("gameblock.php"); ?>
 
-
 		<div style = "clear: both"></div>
 
 	</div>
+
+	<div id = "Footer" style = "text-align: center; font-size: 11px; color: gray;">
+		<p>&copy; 2016 Game Painter, developed by <a href = "http://www.tigrisgames.com/" title = "indepedenent game development studio">Tigris Games</a></p>
+		<p>Contact: <input type = "text" value = "dev@tigrisgames.com" onclick = "this.select();" style = "background: #f1f1f1; border: 1px solid #ddd; padding-left: 4px; height: 14px; font-size: 11px;"/></p>
+	</div>
+
 </body>
 </html>
